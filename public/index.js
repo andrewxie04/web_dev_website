b295aa9b703388a8445605a847eb1c0baf68b5fe
Andrew Xie <andrewxie2004@gmail.com>
2021-03-23 16:16:29 -0400

    added apod

--- public/index.js
+++ public/index.js
@@ -1,69 +1,17 @@
-var express = require( 'express' ) ;
-var app = express() ;
+var express = require('express');
+var app = express();
 
-app.set("view engine", "hbs" );
-app . get( '/' , function( req , res ) 
-{
-   res . render('index');
-} ) ;
+const factsabout = require("../routes/factsabout.js")
+const dogfishcat = require("../routes/dogfishcat.js")
+const apod = require("../routes/apod.js")
 
-app . get( '/test' , function( req , res ) 
-{
-   console . log( 'user landed at page' ) ;
-   res . render('test');
-} ) ;
+app.use("/", factsabout)
+app.use("/", dogfishcat)
+app.use("/", apod)
 
-app . get( '/dog' , function( req , res ) 
-{
-   console . log( 'user landed at page' ) ;
-   res . render('dog');
-} ) ;
+app.set("view engine", "hbs");
 
-app . get( '/cat' , function( req , res ) 
-{
-   console . log( 'user landed at page' ) ;
-   res . render('cat');
-} ) ;
-
-app . get( '/fish' , function( req , res ) 
-{
-   console . log( 'user landed at page' ) ;
-   res . render('fish');
-} ) ;
-
-app . get( '/pet' , function( req , res ) 
-{
-      let type = req.query.type;
-      if('dog' == type){
-         res.render('dog')
-      }
-      if('cat' == type){
-         res.render('cat')
-      }
-      if('fish' == type){
-         res.render('fish')
-      }
-} ) ;
-
-app . get( '/facts' , function( req , res ) 
-{
-      let topic = req.query.topic;
-      let num = req.query.num;
-
-      var arr = [];
-      for(i = 0; i<num; i++) {
-         var st = "";
-         for(j = 0; j<=i; j++){
-            st+=topic
-         }
-         arr.push(st);
-      }
-
-      res.render('facts_about', {arr})
-} ) ;
-
-
-var listener = app . listen( process . env . PORT || 8080 , process . env . HOST || "0.0.0.0" , function() {
-   console . log( "Express server started" ) ;
-} ) ;
+var listener = app.listen(process.env.PORT || 8080, process.env.HOST || "0.0.0.0", function () {
+   console.log("Express server started");
+});
 
