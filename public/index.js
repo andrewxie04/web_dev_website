94e92fe4ef22a35f34049273f1ef891452cc21a9
Andrew Xie <andrewxie2004@gmail.com>
2021-04-13 23:15:51 -0400

    added weather

--- public/index.js
+++ public/index.js
@@ -4,10 +4,12 @@ var app = express();
 const factsabout = require("../routes/factsabout.js")
 const dogfishcat = require("../routes/dogfishcat.js")
 const apod = require("../routes/apod.js")
+const weather = require("../routes/weather.js")
 
 app.use("/", factsabout)
 app.use("/", dogfishcat)
 app.use("/", apod)
+app.use("/", weather)
 
 app.set("view engine", "hbs");
 
